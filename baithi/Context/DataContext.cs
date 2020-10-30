7329512baa89092d61a7cb9793a7cb4af26a9f54
Phạm Ngọc Minh <minhrealmadridcr888@gmail.com>
2020-10-30 16:21:20 +0700

    Practical

---
+++ baithi/Context/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using baithi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace baithi.Context
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("connection") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ baithi/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace baithi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phong_ban { get; set; }
+        public double tienluong { get; set; }
+    }
+}
